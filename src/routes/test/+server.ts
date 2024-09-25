10c1c42e318a517adbb83f78416d617befde8564
Joey <joey.xf@gmail.com>
2024-09-25 17:04:41 +0800

    require is not defined heree

--- src/routes/test/+server.ts
+++ src/routes/test/+server.ts
@@ -1,9 +1,11 @@
 import { json } from '@sveltejs/kit';
+import { ACTIONS_CORS_HEADERS } from '@solana/actions';
 
 export async function GET() {
     const title = 'hello';
-
     return json({
         title,
+    }, {
+        headers: ACTIONS_CORS_HEADERS
     });
 }
